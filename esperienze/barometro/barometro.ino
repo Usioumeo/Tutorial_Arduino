45494a5011f5f54fd26a104585fcbe87796b1242
Alessio <alessio@zeni.info>
2023-02-08 11:58:26 +0100

    test

--- esperienze/barometro/barometro.ino
+++ esperienze/barometro/barometro.ino
@@ -1,9 +1,31 @@
-#include<Arduino_LPS22HB.h>
+#include"BARO.h"
+#include<math.h>
+// importare la libreria dalla cartella locale
 
-void setup(){
-  
+void setup() {
+  //inizializzo la seriale
+  Serial.begin(9600);
+  while (!Serial);
+
+  if (!BARO.begin()) {
+    Serial.println("Impossibile connettersi al sensore!");
+    while (1);
+  }
 }
 
-void loop(){
+void loop() {
+  // leggere il sensore (ritorna la pressione in kPa)
+  float pressure = BARO.readPressure();
   
+  //calcolo l'altitudine dalla pressione
+  float altitude = -293.15*8.31/(9.81*0.02896)*log(pressure/101.3);
+  
+
+  // visualizzo su serial
+  Serial.print("Altitudine in base alla pressione = ");
+  Serial.print(altitude);
+  Serial.println(" m");
+
+  // attendo 1 secondo
+  delay(1000);
 }
